a7a3ae792bd7d1cced43dc4b82e9a3b291a08ca9
lvfengwen <401192595@qq.com>
2017-10-25 19:26:42 +0800

    前台页面首页展示

--- src/components/site/goodslist.vue
+++ src/components/site/goodslist.vue
@@ -1,5 +1,6 @@
 <template>
         <div class="section">
+
             <div class="wrapper">
                 <div class="wrap-box">
                     <!--类别菜单-->
@@ -70,30 +71,85 @@
                     <!--/推荐商品-->
                 </div>
             </div>
+
+            <!--2.0分页商品展示-->
+            <div class="section">
+                <div v-for="(item,index) in clist "  :key="index">
+                    <!--子类-->
+                    <div class="main-tit">
+                    <h2>{{item.catetitle}}</h2>
+                        <p>                     
+                            <a href="/goods/43.html" v-for="(item,index) in item.level2catelist" :key="index">
+                                {{item.subcatetitle}}
+                            </a>
+                   
+                            <a href="/goods/40.html">更多<i>+</i></a>
+                        </p>
+                    </div>
+                    <!--/子类-->
+                    <div class="wrapper clearfix">
+                        <div class="wrap-box">
+                            <ul class="img-list">
+                             
+                                <li  v-for="(item,index) in item.datas">
+                                    <a href="/goods/show-91.html">
+                                        <div class="img-box">
+                                            <img :src="item.img_url">
+                                        </div>
+                                        <div class="info">
+                                            <h3>{{item.artTitle}}</h3>
+                                            <p class="price">
+                                            <b>{{item.sell_price}}</b>元</p>
+                                            <p>
+                                                <strong>库存：{{item.stock_quantity}}</strong>
+                                                <span>市场价：
+                                                <s>{{item.market_price}}</s>
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </a>
+                                </li>                         
+                            </ul>
+                        </div>
+                    </div>
+                 </div>
+            </div>
         </div>
 </template>
 
 <script>
 
-console.log(123);
-
 //当前Vue组件的Vue对象：想要使用Vue，就要写在下面。
   export default{
     data(){
         return {
-            ginfo:{}
+            ginfo:{},
+            //当前页和当前页大小
+            pageIndex:1,
+            pageSize:10,
+            clist:[]
         }
     },
     created(){
         this.getginfo();
+        this.getclist();
     },
     methods: {
        // 获取商品的数据
        getginfo(){
            this.$http.get('/site/goods/gettopdata/goods').then(res=>{
+           console.log(res.data.message);
                  this.ginfo = res.data.message;
-                 console.log(this.ginfo);
+
             });
+        },
+        getclist(){
+            this.$http.get('/site/goods/getgoodsgroup')
+            .then(res=>{
+                this.clist = res.data.message;
+                console.log(this.clist);
+            });
+
         }
     }
   }
@@ -117,5 +173,8 @@ console.log(123);
 
           .el-carousel__item:nth-child(2n+1) {
              background-color: #d3dce6;
+          }
+          .img-list{
+             margin:0 !important;
           }
 </style>
